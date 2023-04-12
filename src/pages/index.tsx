3578b5394fcc04ef9c5ffcf539623b52e29bbf42
johntony366 <johntony366@gmail.com>
2023-04-12 20:12:02 +0530

    fix: solve issue with redirection from index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,18 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useRouter } from "next/router";
-
-const inter = Inter({ subsets: ["latin"] });
+import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/register");
-
-  return (
-    <>
-      <Head>
-        <title>Social Media app</title>
-        <meta
-          name="description"
-          content="Create posts, follow your friends, like posts and more"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
-  );
+  return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // Redirect the user to /register
+  return {
+    redirect: {
+      destination: "/register",
+      permanent: false,
+    },
+  };
+};
