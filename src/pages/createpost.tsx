9d35efebd64c692460304ab7e4f67eaff822175c
johntony366 <johntony366@gmail.com>
2023-04-12 20:59:01 +0530

    fix: add navbar to create post page

--- src/pages/createpost.tsx
+++ src/pages/createpost.tsx
@@ -14,6 +14,7 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
+import Navbar from "@/components/Navbar";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -54,6 +55,7 @@ function Login() {
 
   return (
     <>
+      <Navbar />
       <form onSubmit={handleSubmit}>
         <Flex
           w={"100vw"}
