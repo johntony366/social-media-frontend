30d0d91f8a6a0b3f786f8e327a52d7de013a738b
johntony366 <johntony366@gmail.com>
2023-04-12 18:58:34 +0530

    redirect index page to register page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+  router.push("/register");
+
   return (
     <>
       <Head>
